ccdb60b3c1bba04bf99e482f96c96d835f72f73b
Abraham Ebijuni <abreb1235@gmail.com>
2025-10-29 15:23:28 +0100

    Refactor Footer component for better responsiveness
    Updated footer layout and alignment styles.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="flex gap-x-6 flex-wrap gap-y-16 justify-between items-center px-6 py-2">
-      <a href="https://app.vitalswap.com" target="_blank">
+    <footer className="flex gap-x-6 flex-wrap gap-y-10 md:flex-nowrap justify-center md:justify-between items-center px-6 py-2">
+      <a href="https://app.vitalswap.com" className="mx-auto" target="_blank">
         <img src="/vitalswap_logo_alt.png" className="w-28" />
       </a>
-      <p>
+      <p className=”text-center”>
         Copyright © {new Date().getFullYear()} VitalSwap. All rights reserved.
       </p>
     </footer>
